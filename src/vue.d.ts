cc9795f03b8130979e89c191b25699b884779b3b
jnt0r <38696668+jnt0r@users.noreply.github.com>
2021-01-30 18:13:54 +0100

    add vue.d.ts

---
+++ src/vue.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Extends interfaces in Vue.js
+ */
+
+import { ComponentCustomProperties } from 'vue';
+import AuthProperty from "./AuthProperty";
+
+declare module '@vue/runtime-core' {
+    interface ComponentCustomProperties {
+        $auth: AuthProperty;
+    }
+}
