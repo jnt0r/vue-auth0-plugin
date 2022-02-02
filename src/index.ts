bbd07b687c0a9f8da35cabf6527ba3e01524754b
Hayden Braxton <21197155+haydenbr@users.noreply.github.com>
2022-02-02 23:01:15 +0530

    feat: added injectAuth function for easy injection of Auth0 (#357)

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
-import { App } from 'vue';
+import { App, inject } from 'vue';
 import createAuth0Client, { Auth0ClientOptions } from '@auth0/auth0-spa-js';
 import Plugin from './plugin';
 import AuthenticationGuard from './AuthenticationGuard';
+import AuthProperty from './AuthProperty';
+
+const vueAuthInjectionKey = 'auth'
 
 export default {
     install (app: App, options: Auth0ClientOptions): void {
         // global $auth property is deprecated
         app.config.globalProperties.$auth = Plugin.properties;
-        app.provide('auth', Plugin.properties);
+        app.provide(vueAuthInjectionKey, Plugin.properties);
 
         createAuth0Client(options).then((client) => Plugin.initialize(app, client));
     },
 };
 
+const injectAuth = () => inject<AuthProperty>(vueAuthInjectionKey)
+
 const AuthenticationState = Plugin.state;
 const AuthenticationProperties = Plugin.properties;
-export { AuthenticationGuard, AuthenticationState, AuthenticationProperties };
+export { AuthenticationGuard, AuthenticationState, AuthenticationProperties, injectAuth, vueAuthInjectionKey as injectionKey };
